1db84cc74315d342e5496afa29c4bd50659312e6
berliplan <formales@posteo.de>
2022-10-02 16:11:17 +0200

    Add base grammar

---
+++ grammar/rules.pl
@@ -0,0 +1,27 @@
+% -*-trale-prolog-*-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%   $RCSfile: rules.pl,v $
+%%  $Revision: 1.4 $
+%%      $Date: 2005/10/20 08:17:02 $
+%%     Author: Stefan Mueller (Stefan.Mueller@cl.uni-bremen.de)
+%%    Purpose: 
+%%   Language: Trale
+%      System: TRALE 2.3.7 under Sicstus 3.9.1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+h_arg rule
+(head:Head,
+ subcat:Subcat)
+  ===>
+cat> (head:Head,
+      subcat:[NonheadDtr|Subcat]),
+cat> NonheadDtr.
+
+arg_h rule
+(head:Head,
+ subcat:Subcat)
+  ===>
+cat> NonheadDtr,
+cat> (head:Head,
+      subcat:[NonheadDtr|Subcat]).
---
+++ grammar/syntax.pl
@@ -0,0 +1,19 @@
+% -*-trale-prolog-*-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%   $RCSfile: syntax.pl,v $
+%%  $Revision: 1.3 $
+%%      $Date: 2005/10/20 08:17:02 $
+%%     Author: Stefan Mueller (Stefan.Mueller@cl.uni-bremen.de)
+%%    Purpose: 
+%%   Language: Trale
+%      System: TRALE 2.3.7 under Sicstus 3.9.1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+root macro
+ (subcat:[]).
+
+interrog macro
+ (@root).
+
+decl macro
+ (@root).
