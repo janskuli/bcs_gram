46a042fa89b55fce0fda200487830defb4336ac9
János Litzinger <janos.litzinger@hu-berlin.de>
2023-01-13 17:49:22 +0100

    Harmonize identation

--- grammar/lexicon.pl
+++ grammar/lexicon.pl
@@ -9,187 +9,236 @@
 %      System: TRALE 2.3.7 under Sicstus 3.9.1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-vladika ~~>
- (word,
+vladika ~~> (word,
   head:(noun,
-        agr:(Agr,
-             per:(Per,third),
-	         gen:(Gen,mas),
-             num:(Num,sg))),
-     spr:[(head:(det,
-                 agr:Agr),
-             sem:index:Ind)],
-     subcat:[],
- sem:(index:(Ind,
-       object,
-       per:Per,
-       gen:Gen,
-       num:Num
-       ),
-       key:(Key,
-            pred:(a_ vladika_rel),
-            arg0:Ind),
-       rels:[Key])).
+    agr:(Agr,
+      per:(Per,third),
+      gen:(Gen,mas),
+      num:(Num,sg)
+    )
+  ),
+  spr:[(
+    head:(det,
+      agr:Agr),
+    sem:index:Ind
+  )],
+  subcat:[],
+  sem:(
+    index:(Ind,
+      object,
+      per:Per,
+      gen:Gen,
+      num:Num
+    ),
+    key:(Key,
+      pred:(a_ vladika_rel),
+      arg0:Ind
+    ),
+    rels:[Key]
+   )
+).
 
-vladike ~~>
- (word,
+vladike ~~> (word,
   head:(noun,
-        agr:(Agr,
-             per:third,
-	         gen:fem_mas,
-             num:pl)),
-     spr:[(head:(det,
-                 agr:Agr),
-             sem:index:Ind)],
-     subcat:[],
- sem:(index:(Ind,
-       object,
-       per:third,
-       gen:mas,
-       num:pl),
-       key:(Key,
-            pred:(a_ vladike_rel),
-            arg0:Ind),
-       rels:[Key])).
+    agr:(Agr,
+      per:third,
+      gen:fem_mas,
+      num:pl
+    )
+  ),
+  spr:[(
+    head:(det,
+      agr:Agr),
+    sem:index:Ind
+  )],
+  subcat:[],
+  sem:(
+    index:(Ind,
+      object,
+      per:third,
+      gen:mas,
+      num:pl
+    ),
+    key:(Key,
+      pred:(a_ vladike_rel),
+      arg0:Ind
+    ),
+    rels:[Key]
+  )
+).
 
-vladike ~~>
- (word,
+vladike ~~> (word,
   head:(noun,
-        agr:(
-             per:third,
-	         gen:(Gen,fem_mas),
-             num:pl)),
-     spr:[(head:(det,
-                 agr:(
-                 gen:Gen,
-                 num:Num
-             )),
-             sem:index:Ind)],
-     subcat:[],
- sem:(index:(Ind,
-       object,
-       per:Per,
-       gen:(Gen,mas),
-       num:(Num,pl)),
-       key:(Key,
-            pred:(a_ vladike_rel),
-            arg0:Ind),
-       rels:[Key])).
+    agr:(
+      per:third,
+      gen:(Gen,fem_mas),
+      num:pl
+    )
+  ),
+  spr:[(
+    head:(det,
+      agr:(
+        gen:Gen,
+        num:Num
+      )
+    ),
+    sem:index:Ind
+  )],
+  subcat:[],
+  sem:(
+    index:(Ind,
+      object,
+      per:Per,
+      gen:(Gen,mas),
+      num:(Num,pl)
+    ),
+    key:(Key,
+      pred:(a_ vladike_rel),
+      arg0:Ind
+    ),
+    rels:[Key]
+  )
+).
+
 % ADJECTIVES
 %Singular
-stara ~~>
-(word,
+stara ~~> (word,
  head:(adj,
-       agr:Agr,
-       mod:[
-           (head:(noun,
-                  agr:(Agr,
-                       num:Num,
-                       gen:Gen
-            )),
-            spr:[_],
-            sem:index:(Ind,
-                       num:(Num,sg),
-                       gen:(Gen,fem)
-                   )
-            )],
-       pre_modifier:plus
+   agr:Agr,
+   mod:[(
+     head:(noun,
+       agr:(Agr,
+         num:Num,
+         gen:Gen
+       )
+     ),
+     spr:[_],
+     sem:index:(Ind,
+           num:(Num,sg),
+           gen:(Gen,fem)
+         )
+   )],
+   pre_modifier:plus
  ),
- sem:(index:Ind,
-       key:(Key,
-            pred:(a_ stara_rel),
-            arg0:Ind),
-       rels:[Key]),
+ sem:(
+   index:Ind,
+   key:(Key,
+     pred:(a_ stara_rel),
+     arg0:Ind
+   ),
+   rels:[Key]),
   spr:[],
-  subcat:[]).
+  subcat:[]
+).
 
 %Plural
-stari ~~>
-(word,
- head:(adj,
-       agr:Agr,
-       mod:[
-           (head:(noun,
-                  agr:(Agr,
-                       num:Num,
-                       gen:Gen
-            )),
-            spr:[_],
-            sem:index:(Ind,
-                       num:Num,
-                       gen:(Gen,mas)
-                   )
-            )],
-       pre_modifier:plus
- ),
- sem:(index:Ind,
-       key:(Key,
-            pred:(a_ stari_rel),
-            arg0:Ind),
-       rels:[Key]),
+stari ~~>(word,
+  head:(adj,
+    agr:Agr,
+    mod:[(
+      head:(noun,
+        agr:(Agr,
+          num:Num,
+          gen:Gen
+        )
+      ),
+      spr:[_],
+      sem:index:(Ind,
+          num:Num,
+          gen:(Gen,mas)
+        )
+      )],
+    pre_modifier:plus
+  ),
+  sem:(
+    index:Ind,
+    key:(Key,
+      pred:(a_ stari_rel),
+      arg0:Ind
+    ),
+    rels:[Key]
+  ),
   spr:[],
-  subcat:[]).
+  subcat:[]
+).
 
-stare ~~>
-(word,
- head:(adj,
-       agr:Agr,
-       mod:[
-           (head:(noun,
-                  agr:(Agr,
-                       num:pl,
-                       gen:fem_mas
-            )),
-            spr:[_],
-            sem:index:Ind
-            )],
-       pre_modifier:plus
- ),
- sem:(index:Ind,
-       key:(Key,
-            pred:(a_ stare_rel),
-            arg0:Ind),
-       rels:[Key]),
+stare ~~>(word,
+  head:(adj,
+    agr:Agr,
+    mod:[(
+      head:(noun,
+        agr:(Agr,
+          num:pl,
+          gen:fem_mas
+        )
+      ),
+      spr:[_],
+      sem:index:Ind
+    )],
+    pre_modifier:plus
+  ),
+  sem:(
+    index:Ind,
+    key:(Key,
+      pred:(a_ stare_rel),
+      arg0:Ind
+    ),
+    rels:[Key]
+  ),
   spr:[],
-  subcat:[]).
+  subcat:[]
+).
 
 oni ~~> (word,
   head:(det,
-        agr:(gen:mas,
-             num:pl)),
-  sem:(index:Ind,
-       key:(Key,
-            pred:(a_ oni_rel),
-            arg0:Ind),
-       rels:[Key]),
+    agr:(
+      gen:mas,
+      num:pl
+    )
+  ),
+  sem:(
+    index:Ind,
+    key:(Key,
+      pred:(a_ oni_rel),
+      arg0:Ind
+    ),
+    rels:[Key]
+  ),
   spr:[],
-  subcat:[]).
+  subcat:[]
+).
 
 one ~~> (word,
   head:(det,
-        agr:(gen:fem,
-             num:pl)),
-  sem:(index:Ind,
-       key:(Key,
-            pred:(a_ one_rel),
-            arg0:Ind),
-       rels:[Key]),
+    agr:(
+      gen:fem,
+      num:pl
+    )
+  ),
+  sem:(
+    index:Ind,
+    key:(Key,
+      pred:(a_ one_rel),
+      arg0:Ind
+    ),
+    rels:[Key]
+  ),
   spr:[],
-  subcat:[]).
+  subcat:[]
+).
 
 posvađale ~~> (word,
   head:verb,
-  subcat:[
-    (head:(noun,
-       agr:(
-         num:pl,
-         gen:fem
-       )
-     ),
-     sem:index:Arg1,
-     subcat:[]
-    )
-  ],
+  subcat:[(
+    head:(noun,
+      agr:(
+        num:pl,
+        gen:fem
+      )
+    ),
+    sem:index:Arg1,
+    subcat:[]
+  )],
   sem:(
     index:(Ind,event),
     key:(Key,
@@ -204,19 +253,18 @@ posvađale ~~> (word,
 
 posvađali ~~> (word,
   head:verb,
-  subcat:[
-          (head:(noun,
-             agr:(
-               num:pl,
-               gen:fem_mas
-             )
-           ),
-           sem:index:(Arg1,
-                 gen:mas
-                 ),
-           subcat:[]
-          )
-         ],
+  subcat:[(
+    head:(noun,
+      agr:(
+        num:pl,
+        gen:fem_mas
+      )
+    ),
+    sem:index:(Arg1,
+          gen:mas
+          ),
+    subcat:[]
+  )],
   sem:(
     index:(Ind,event),
     key:(Key,
